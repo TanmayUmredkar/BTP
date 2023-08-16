a947bb6e20f97874ab25285341b39d5d6a9a232a
TanmayUmredkar <umredkartanmay32@gmail.com>
2023-08-16 12:26:28 +0530

    Simon

---
+++ Simon/recieve.py
@@ -0,0 +1,43 @@
+# Receiver.py
+import simon
+import binascii
+import sys
+import socket
+
+# Create a socket connection
+receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+receiver_ip = 'RECEIVER_IP_ADDRESS'
+receiver_port = RECEIVER_PORT_NUMBER
+receiver_address = (receiver_ip, receiver_port)
+receiver_socket.bind(receiver_address)
+receiver_socket.listen(1)
+print("Waiting for connection...")
+connection, sender_address = receiver_socket.accept()
+print("Connected to", sender_address)
+
+# Receive key and encrypted message
+key_str = connection.recv(1024).decode('utf-8')
+m_str = connection.recv(1024).decode('utf-8')
+
+# Convert key and message to integers
+key = int(key_str, 16)
+m = int(m_str, 10)
+
+# Close the connection
+connection.close()
+
+# Decryption process
+ksize = (len(key_str) - 2) * 4
+bsize = 32
+if ksize == 72:
+    bsize = 48
+elif ksize == 96:
+    bsize = 48
+elif ksize == 128:
+    bsize = 64
+
+w = simon.SimonCipher(key, key_size=ksize, block_size=bsize)
+
+res = w.decrypt(m)
+res_str = bytes.fromhex(hex(res)[2:]).decode('utf-8')
+print("Decrypted:", res_str)
---
+++ Simon/send.py
@@ -0,0 +1,33 @@
+# Sender.py
+import simon
+import binascii
+import sys
+import socket
+
+# Encryption key and plaintext message
+k = '0x1b1a1918131211100b0a090803020100'
+mess = 'hello'
+
+# Convert message to binary
+def getBinary(word):
+    return int(binascii.hexlify(word), 16)
+
+# Convert plaintext message to binary
+m = getBinary(mess)
+
+# Convert key to integer
+key = int(k, 16)
+
+# Create a socket connection
+sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+receiver_ip = 'RECEIVER_IP_ADDRESS'
+receiver_port = RECEIVER_PORT_NUMBER
+receiver_address = (receiver_ip, receiver_port)
+sender_socket.connect(receiver_address)
+
+# Send key and encrypted message to receiver
+sender_socket.sendall(bytes(str(key), 'utf-8'))
+sender_socket.sendall(bytes(str(m), 'utf-8'))
+
+# Close the socket
+sender_socket.close()
